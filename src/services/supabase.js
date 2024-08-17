61591c76ddf78b85bb71759284ba7df7be5aa615
poouryaaDx <pourya.swe@gmail.com>
2024-08-17 12:37:42 +0330

    Add .env to gitigonre & changed supabse config to env cred

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "<Your Rest API from SupaBase"; /// REST API
-
-const supabaseKey = "<Your Public Key From SupaBase>"; /// Public Key
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_REST_API; ///Your REST API From SupaBase - Stirng -
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLIC_KEY; ///Your Public Key From SupaBase - String -
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
